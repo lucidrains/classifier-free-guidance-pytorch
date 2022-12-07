c0d2a3e737288514ebd5921eed04f1a1813037de
Phil Wang <lucidrains@gmail.com>
2022-12-07 13:09:43 -0800

    need attention

--- classifier_free_guidance_pytorch/classifier_free_guidance_pytorch.py
+++ classifier_free_guidance_pytorch/classifier_free_guidance_pytorch.py
@@ -6,6 +6,79 @@ from einops import rearrange, repeat
 from classifier_free_guidance_pytorch.open_clip import OpenClipAdapter
 from classifier_free_guidance_pytorch.t5 import t5_encode_text
 
+# attention
+
+class Attention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        dim_head = 64,
+        heads = 8,
+        dim_context = None,
+        norm_context = False,
+        num_null_kv = 0
+    ):
+        super().__init__()
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        self.causal = causal
+        inner_dim = dim_head * heads
+
+        dim_context = default(dim_context, dim)
+
+        self.norm = LayerNorm(dim)
+        self.context_norm = LayerNorm(dim_context) if norm_context else nn.Identity()
+
+        self.num_null_kv = num_null_kv
+        self.null_kv = nn.Parameter(torch.randn(2, num_null_kv, dim_head))
+
+        self.to_q = nn.Linear(dim, inner_dim, bias = False)
+        self.to_kv = nn.Linear(dim_context, dim_head * 2, bias = False)
+        self.to_out = nn.Linear(inner_dim, dim, bias = False)
+
+    def forward(
+        self,
+        x,
+        context = None,
+        mask = None
+    ):
+        b = x.shape[0]
+
+        if exists(context):
+            context = self.context_norm(context)
+
+        kv_input = default(context, x)
+
+        x = self.norm(x)
+
+        q, k, v = self.to_q(x), *self.to_kv(kv_input).chunk(2, dim = -1)
+
+        if self.num_null_kv > 0:
+            null_k, null_v = repeat(self.null_kv, 'kv n d -> kv b n d', b = b).unbind(dim = 0)
+            k = torch.cat((null_k, k), dim = -2)
+            v = torch.cat((null_v, v), dim = -2)
+
+        q = rearrange(q, 'b n (h d) -> b h n d', h = self.heads)
+
+        q = q * self.scale
+
+        sim = einsum('b h i d, b j d -> b h i j', q, k)
+
+
+        if exists(mask):
+            mask = F.pad(mask, (self.num_null_kv, 0), value = True)
+            mask = rearrange(mask, 'b j -> b 1 1 j')
+            sim = sim.masked_fill(~mask, -torch.finfo(sim.dtype).max)
+
+        attn = sim.softmax(dim = -1)
+
+        out = einsum('b h i j, b j d -> b h i d', attn, v)
+
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+# text conditioning
+
 class TextConditioner(nn.Module):
     def __init__(
         self,
