c6c50c89dd9fe7980ab9f2f9b69d98e56022c869
Phil Wang <lucidrains@gmail.com>
2022-12-15 13:13:16 -0800

    expose the classifier free guidance decorator

--- classifier_free_guidance_pytorch/__init__.py
+++ classifier_free_guidance_pytorch/__init__.py
@@ -1,4 +1,5 @@
 from classifier_free_guidance_pytorch.classifier_free_guidance_pytorch import TextConditioner
+from classifier_free_guidance_pytorch.classifier_free_guidance_pytorch import classifier_free_guidance
 
 from classifier_free_guidance_pytorch.open_clip import OpenClipAdapter
 from classifier_free_guidance_pytorch.t5 import T5Adapter
