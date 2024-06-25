3a0822a41b1bda2cf14bfc1f50925cb816891c47
Marcus Loren <65302107+MarcusLoppe@users.noreply.github.com>
2024-06-25 20:22:12 +0200

    Update open_clip.py

--- classifier_free_guidance_pytorch/open_clip.py
+++ classifier_free_guidance_pytorch/open_clip.py
@@ -11,7 +11,7 @@ from classifier_free_guidance_pytorch.tokenizer import tokenizer
 # constants
 
 DEFAULT_CLIP_NAME = 'ViT-B-32'
-DEFAULT_PRETRAINED_CLIP = 'laion400m_e32'
+DEFAULT_PRETRAINED_CLIP = 'openai'
 
 # helper functions
 
